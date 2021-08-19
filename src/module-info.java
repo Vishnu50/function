cfb8e91b1bab2aca4831478ab94ace2ac379bf31
vishnu <vishnu.v>
2021-08-19 13:05:04 +0530

    code change

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+import java util.scanner
+Public class Main{
+Public class void announceDeveloperTeatime()
+{
+	system.out.println("waiting for tea time");
+	system.out.println("type in random");
+	Scanner input=new Scanner(System.in);
+	input.next();
+	system.out.println("Its a tea time")
+}
+public static void main (string[] args) {
+	announceDeveloperTeatime();
+}
